f30a92ee84115f1634481561d5ab20d048108b70
Matthew Palmer <81492324+zy538324@users.noreply.github.com>
2025-04-19 20:58:07 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -130,25 +130,25 @@ Database:
   path: media_management.db
   track_modifications: false
 Jackett:
-  api_key: your_jackett_api_key_here
+  api_key: j5bmf5mf3m83yx12tx3wbxk9jarux2wd
   categories:
     Movies: '2000'
     Music: '3000'
     TV: '5000'
-  server_url: http://your_jackett_server:9117/
+  server_url: http://10.252.0.15:9117/
 Jellyfin:
-  api_key: your_jellyfin_api_key_here
-  server_url: http://your_jellyfin_server:8096
-Secret_key: $(openssl rand -base64 24)
+  api_key: d60eb1a6c2594fa7a47924c1b37cf81c
+  server_url: http://10.252.0.155:8096
+Secret_key: ZV}1Ux/!LfbACbIR@0:F
 Spotify:
-  client_id: your_spotify_client_id_here
-  client_secret: your_spotify_client_secret_here
+  client_id: b3fb8ab712a9449e94e70c77d5ba5426
+  client_secret: f6e5c9c048c6453482d3cef2fd74b7ae
 TMDb:
-  api_key: your_tmdb_api_key_here
+  api_key: 84147d5f9aa77e54c45cd5b6ceabd0fc
 qBittorrent:
-  host: http://your_qbittorrent_host:8080
-  password: your_qbittorrent_password
-  username: your_qbittorrent_username
+  host: http://10.252.0.15:8080
+  password: =hX-z`PB_'4BiY'R+E~\
+  username: admin
 EOF
     chown $APP_USER:$APP_GROUP $CONFIG_DIR/config.yaml
     chmod 600 $CONFIG_DIR/config.yaml
